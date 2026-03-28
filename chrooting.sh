d7fb769f3747bf9ae3c8212591512e00edbafaef
Nigromante <nigromante@github.com>
2026-03-28 16:56:20 -0300

    support for rescue folder

--- chrooting.sh
+++ chrooting.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
+scriptdir() {                                                          ┊       │
+    local SCRIPTPATH=$(realpath "$0")
+    SCRIPTDIR=$(dirname "$SCRIPTPATH")
+    echo "$SCRIPTDIR"
+}
 
 src="/dev/sdb2"
-echo "[$src]"
-
 target="/mnt/otherlinux"
-echo "[$target]"
+curdir=$( scriptdir )
+tools="${curdir}/rescue"
+clear
 
+
+echo "[[ $curdir ]]"
+echo "[[ $tools ]]"
+echo "[$src]"
+echo "[$target]"
 
 if [ ! -e "$target" ]; then
   echo creating $target
-  mkdir -p $target
+  mkdir -p "$target/rescue"
 fi
 
 cmd=" sudo  umount  $src "
@@ -21,16 +31,26 @@ cmd=" sudo  mount  $src  ${target} "
 echo $cmd
 eval $cmd
 
-uname -nr
 
-eval "sudo mount --bind /dev      $target/dev"
-eval "sudo mount --bind /dev/pts  $target/dev/pts"
-eval "sudo mount --bind /proc     $target/proc"
-eval "sudo mount --bind /sys      $target/sys"
-eval "sudo mount --bind /run      $target/run"
+for dev in /dev /dev/pts /proc /sys /run; do
+  cmd="sudo mount --bind $dev  ${target}${dev} "
+  echo $cmd
+  eval $cmd
+done
+
+cmd="sudo mount --bind $tools  ${target}/mnt/rescue "
+echo $cmd
+eval $cmd
+
+echo
+echo /mnt/rescue
+echo
+echo -n "Host system : "; uname -nr
+echo
 
 cmd=" sudo  chroot  ${target} "
 echo $cmd
+echo
 eval $cmd
 
 cmd=" sudo  umount -Rf   ${target} "
